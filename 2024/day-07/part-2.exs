f40ddd710d61a932e085aeaa9494a1b7ff1579fb
EthanEFung <Ethanefung@gmail.com>
2024-12-11 21:48:39 -0800

    2024 day 07

---
+++ 2024/day-07/part-2.exs
@@ -0,0 +1,60 @@
+example = "
+190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20
+" |> String.trim()
+
+input =
+  case File.read("input.txt") do
+    {:ok, content} -> content |> String.trim()
+  end
+
+defmodule Part2 do
+  def parse([result, values]) do
+    {
+      String.to_integer(result),
+      String.split(values) |> Enum.map(&String.to_integer/1)
+    }
+  end
+
+  def cat(a, b) do
+    (Integer.digits(a) ++ Integer.digits(b)) |> Integer.undigits()
+  end
+
+  def valid({result, [final]}) do
+    result == final
+  end
+
+  def valid({result, [this, that]}) do
+    valid({result, [this + that]}) ||
+      valid({result, [this * that]}) ||
+      valid({result, [cat(this, that)]})
+  end
+
+  def valid({result, [this, that | others]}) do
+    valid({result, [this + that | others]}) ||
+      valid({result, [this * that | others]}) ||
+      valid({result, [cat(this, that) | others]})
+  end
+
+  def total(list) do
+    for {result, _} <- list do
+      result
+    end
+    |> Enum.sum()
+  end
+end
+
+input
+|> String.split("\n")
+|> Enum.map(&String.split(&1, ": "))
+|> Enum.map(&Part2.parse/1)
+|> Enum.filter(&Part2.valid/1)
+|> Part2.total()
+|> IO.inspect(label: "wow...thats some fast recursion")
